74fbb2303897f5e761b882791d331dbc6561bb59
blade08 <cc411360770@sina.com>
2016-09-01 07:42:24 +0800

    数据库脚本

---
+++ iServer/dbscript/02.data.sql
@@ -0,0 +1,7 @@
+
+
+INSERT INTO `t_siteuser` (`id`, `userName`, `name`, `password`, `authorities`, `enabled`, `age`, `sex`) VALUES
+
+  ('001','chencheng','陈澄','Huawei123','2','1','27','0');
+COMMIT;
+
