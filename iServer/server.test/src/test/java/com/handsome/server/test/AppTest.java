195f70bcdc42f897fcf55add1a1079b523407358
blade08 <cc411360770@sina.com>
2016-09-23 00:11:23 +0800

    测试工程提交

---
+++ iServer/server.test/src/test/java/com/handsome/server/test/AppTest.java
@@ -0,0 +1,42 @@
+package com.handsome.server.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.handsome.product.api.bean.Product;
+import com.handsome.product.api.service.ProductService;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations =
+{
+		"classpath:spring/applicationContext_bean.xml",
+		"classpath:spring/applicationContext_hessian.xml" })
+//@WebAppConfiguration
+public class AppTest
+{
+	@Autowired
+	private ApplicationContext ac;
+
+	@Test
+	public void test()
+	{
+
+	}
+
+	@Test
+	public void testApplication()
+	{
+		ProductService productService = (ProductService) ac
+				.getBean("productService");
+		System.out.println(productService.toString());
+		productService.addProduct(new Product());
+
+	}
+}
