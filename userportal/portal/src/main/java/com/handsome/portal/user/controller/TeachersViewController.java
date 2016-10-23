bf53526f4061cb8426b12a91514a0cb5afeb021f
blade08 <cc411360770@sina.com>
2016-10-23 17:49:44 +0800

    返回详细页面携带数据

--- userportal/portal/src/main/java/com/handsome/portal/user/controller/TeachersViewController.java
+++ userportal/portal/src/main/java/com/handsome/portal/user/controller/TeachersViewController.java
@@ -3,10 +3,17 @@
  */
 package com.handsome.portal.user.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.handsome.portal.user.bean.Teacher;
+import com.handsome.siteuser.api.bean.SiteUser;
+import com.handsome.siteuser.api.service.SiteUserService;
 
 /**
  * 
@@ -22,6 +29,10 @@ public class TeachersViewController
 	{
 		// TODO Auto-generated constructor stub
 	}
+	
+	@Autowired
+	@Qualifier(value = "siteUserService")
+	private SiteUserService siteUserService;
 	
 	/**
 	 * 列表页面
@@ -40,10 +51,14 @@ public class TeachersViewController
 	 * @return ModelAndView
 	 */
 	@RequestMapping(value = "/teacherDetail.do", method = RequestMethod.GET)
-	public ModelAndView teacherDetail()
+	public ModelAndView teacherDetail(@ModelAttribute Teacher teacher)
 	{
 		ModelAndView view = new ModelAndView();
 		view.setViewName("/user/teacherdetail");
+		
+		SiteUser siteUser = siteUserService.getSiteUserByUserName(teacher
+				.getAccount());
+		view.addObject("obj", siteUser);
 		return view;
 	}
 
